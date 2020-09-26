2ee9cb1a8d21cd88b8f9ddc0916246ce57a03978
Danilo La Colla <dalaco618@gmail.com>
2020-09-26 17:34:19 -0300

    commit del conflicto

--- Prueba.java
+++ Prueba.java
@@ -2,7 +2,7 @@
 public class Prueba {
 	
 	public static void main(String[] args) {
-		Persona p= new Persona("Danilo");
+		Persona p= new Persona("Martin");
 		System.out.println("Nombre: "+ p.getName());
 	}
 }
