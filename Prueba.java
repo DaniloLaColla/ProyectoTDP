4019b196a1adfe3d3617e5d93c27ffc963ba6392
Danilo La Colla <dalaco618@gmail.com>
2020-09-26 17:29:15 -0300

    modifique Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -2,7 +2,7 @@
 public class Prueba {
 	
 	public static void main(String[] args) {
-		Persona p= new Persona("Danilo");
+		Persona p= new Persona("Fede");
 		System.out.println("Nombre: "+ p.getName());
 	}
 }
